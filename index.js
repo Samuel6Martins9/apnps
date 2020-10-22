1d81154eb65c6210b56f4968786122525538b7bd
Samuel Martins <samukams100@gmail.com>
2020-10-22 10:03:57 -0300

    Rota sobre

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/mensagem', function(req,res){
     res.send('Essa mensagem é automática')
 })
 
+app.get('/sobre', function(req,res){
+    res.send('Está página está sendo desenvolvida por Samuel')
+})
+
 app.listen(3000,function(){
     console.log('Conexão inicializada')
 })

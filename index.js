32b570dd1c8c6635054cad4c5adeb5c693d560dc
Samuel Martins <samukams100@gmail.com>
2020-10-22 10:32:55 -0300

    Rota Layout

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/mensagem', function(req,res){
     res.send('Essa mensagem é automática')
 })
 
+app.get('/layout', function(req,res){
+    res.send('Layout')
+})
+
 app.listen(3000,function(){
     console.log('Conexão inicializada')
 })

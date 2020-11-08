ef6926a947769f7303ff487cb4ccc9414f6f90e9
Samuel Martins <samukams100@gmail.com>
2020-11-08 20:41:10 -0300

    usando ejs

--- index.js
+++ index.js
@@ -14,7 +14,18 @@ app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "public")))
 
 app.get('/', function(req,res){
-    res.send('Olá Samuel')
+    res.render('index.ejs',{})
+})
+
+app.get('/usuarios',function(req,res){
+    res.render('usuarios.ejs', { usuarios:[
+        {nome:'Samuel',email:'samueldsm100@outlook.com'},
+        {nome:'Davi',email:'sdsdavi@gmail.com'},
+        {nome:'Rodrigo',email:'rodrigorodr@hotmail.com'},
+        {nome:'João',email:'joaoo@outlook.com'},
+        {nome:'Maria',email:'maria@gmail.com'},
+        {nome:'Ana',email:'anasp@gmail.com'}
+    ] })
 })
 
 app.listen(3000,function(){
